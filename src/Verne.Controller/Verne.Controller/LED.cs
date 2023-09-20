d2a81ae308fc5661bb9f41f8f6f5cb08ecca236c
Pete Field <peter.field@newday.co.uk>
2023-09-20 20:18:47 +0100

    Add Controller Web Page

---
+++ src/Verne.Controller/Verne.Controller/LED.cs
@@ -0,0 +1,21 @@
+﻿namespace Verne.Controller
+{
+    public class LED
+    {
+        public LED(int index, string name, bool state = false, bool enabled = true)
+        {
+			Index = index;
+			Name = name;
+			State = state;
+			Enabled = enabled;
+		}
+
+        public int Index { get; set; }
+
+        public string Name { get; set; }
+
+        public bool State { get; set; } = false;
+
+        public bool Enabled { get; set; } = true;
+    }
+}
